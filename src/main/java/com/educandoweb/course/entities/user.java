9745c115ce2ff8a43a8d57688378382db011790b
Glauco Marques Amorim <e-gamorim@tap.pt>
2024-03-28 02:35:55 +0000

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/user.java
+++ src/main/java/com/educandoweb/course/entities/user.java
@@ -1,21 +1,38 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
-public class user implements Serializable  {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name = "tb_user")
+public class user implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private long id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public void User() {		
+	
+	
+	public user() {
 	}
 
-	public user(long id, String name, String email, String phone, String password) {
+	public user(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -24,11 +41,11 @@ public class user implements Serializable  {
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -64,9 +81,16 @@ public class user implements Serializable  {
 		this.password = password;
 	}
 
+
+
+	
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -78,9 +102,11 @@ public class user implements Serializable  {
 		if (getClass() != obj.getClass())
 			return false;
 		user other = (user) obj;
-		return id == other.id;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
 }
